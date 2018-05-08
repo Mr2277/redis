a1865b4ea1bce5a1d6c0040b6ac40d7094ff9c9d
master <837544713@qq.com>
2018-05-08 09:57:01 +0800

    template

--- src/main/java/com/test/mysql/RunMysql.java
+++ src/main/java/com/test/mysql/RunMysql.java
@@ -5,11 +5,19 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 import javax.xml.transform.Templates;
+import java.util.List;
 
 public class RunMysql {
    public static void main(String[] args){
        ApplicationContext ioc = new ClassPathXmlApplicationContext("bean.xml");
        JdbcTemplate templates=ioc.getBean(JdbcTemplate.class);
-       templates.execute("");
+       System.out.println(templates.toString());
+       //templates.execute("");
+       //templates.execute("show tables");
+       List list=templates.queryForList("select * from departments");
+       System.out.println(list.size());
+       for(int i=0;i<list.size();i++){
+           System.out.println(list.get(i));
+       }
    }
 }
