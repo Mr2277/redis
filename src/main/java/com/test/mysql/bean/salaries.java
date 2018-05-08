27551cf6a94c5a916914b4cd73a81e0c39e42ac2
master <837544713@qq.com>
2018-05-08 10:34:13 +0800

    bean

---
+++ src/main/java/com/test/mysql/bean/salaries.java
@@ -0,0 +1,40 @@
+package com.test.mysql.bean;
+
+public class salaries {
+    private String emp_no;
+    private String salary;
+    private String from_date;
+    private String to_date;
+
+    public String getEmp_no() {
+        return emp_no;
+    }
+
+    public void setEmp_no(String emp_no) {
+        this.emp_no = emp_no;
+    }
+
+    public String getSalary() {
+        return salary;
+    }
+
+    public void setSalary(String salary) {
+        this.salary = salary;
+    }
+
+    public String getFrom_date() {
+        return from_date;
+    }
+
+    public void setFrom_date(String from_date) {
+        this.from_date = from_date;
+    }
+
+    public String getTo_date() {
+        return to_date;
+    }
+
+    public void setTo_date(String to_date) {
+        this.to_date = to_date;
+    }
+}
---
+++ src/main/java/com/test/mysql/bean/titles.java
@@ -0,0 +1,40 @@
+package com.test.mysql.bean;
+
+public class titles {
+    private String emp_no;
+    private String title;
+    private String from_date;
+    private String to_date;
+
+    public String getEmp_no() {
+        return emp_no;
+    }
+
+    public void setEmp_no(String emp_no) {
+        this.emp_no = emp_no;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getFrom_date() {
+        return from_date;
+    }
+
+    public void setFrom_date(String from_date) {
+        this.from_date = from_date;
+    }
+
+    public String getTo_date() {
+        return to_date;
+    }
+
+    public void setTo_date(String to_date) {
+        this.to_date = to_date;
+    }
+}
