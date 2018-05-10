fab7750faedb57c0de152278bf62354944206c42
master <837544713@qq.com>
2018-05-10 12:12:17 +0800

    关联关系java  bean

---
+++ src/main/java/com/test/mysql/model/Dept_emp.java
@@ -0,0 +1,41 @@
+package com.test.mysql.model;
+
+public class Dept_emp {
+    private String emp_no;
+    //关联字段 dept——no
+    private Departments departments;
+    private String from_date;
+    private String to_date;
+
+    public String getEmp_no() {
+        return emp_no;
+    }
+
+    public void setEmp_no(String emp_no) {
+        this.emp_no = emp_no;
+    }
+
+    public Departments getDepartments() {
+        return departments;
+    }
+
+    public void setDepartments(Departments departments) {
+        this.departments = departments;
+    }
+
+    public String getFrom_date() {
+        return from_date;
+    }
+
+    public void setFrom_date(String from_date) {
+        this.from_date = from_date;
+    }
+
+    public String getTo_date() {
+        return to_date;
+    }
+
+    public void setTo_date(String to_date) {
+        this.to_date = to_date;
+    }
+}
---
+++ src/main/java/com/test/mysql/model/Dept_manager.java
@@ -0,0 +1,7 @@
+package com.test.mysql.model;
+
+public class Dept_manager {
+    //关联字段dept_no
+    private Departments departments;
+
+}
