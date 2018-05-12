f38e689482eba59bf9d44b19c2e1f71906bb837b
master <837544713@qq.com>
2018-05-12 17:44:56 +0800

    proxy spring

---
+++ src/main/java/com/test/spring/aop/CustomerBo.java
@@ -0,0 +1,11 @@
+package com.test.spring.aop;
+
+public interface CustomerBo {
+    void addCustomer();
+
+    String addCustomerReturnValue();
+
+    void addCustomerThrowException() throws Exception;
+
+    void addCustomerAround(String name);
+}
